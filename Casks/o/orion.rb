54f799c272a88ec20ea51903b9006b66a6511f4c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-27 02:04:54 +0000

    orion 1.1.1

--- Casks/o/orion.rb
+++ Casks/o/orion.rb
@@ -1,6 +1,6 @@
 cask "orion" do
   on_catalina :or_older do
-    version "1.1,148"
+    version "1.1.1,149"
     sha256 "c2d55b9ffa9531e868c76f6fae9fc6ae37420e74238b339c48e5e305ebb4bf96"
 
     url "https://cdn.kagi.com/updates/10_15/#{version.csv.second}.zip"
@@ -11,7 +11,7 @@ cask "orion" do
     end
   end
   on_big_sur do
-    version "1.1,148"
+    version "1.1.1,149"
     sha256 "e68a8270ac308ea8a808dd5b1dbfe518cb0103603ac9adccea37529fc51e1860"
 
     url "https://cdn.kagi.com/updates/11_0/#{version.csv.second}.zip"
@@ -22,7 +22,7 @@ cask "orion" do
     end
   end
   on_monterey do
-    version "1.1,148"
+    version "1.1.1,149"
     sha256 "ff9ac1c197599e126b028619be3d451906f7bc10b615c311de55f2a5685bd631"
 
     url "https://cdn.kagi.com/updates/12_0/#{version.csv.second}.zip"
@@ -33,7 +33,7 @@ cask "orion" do
     end
   end
   on_ventura do
-    version "1.1,148"
+    version "1.1.1,149"
     sha256 "307fd25815c9343d4c18190d94d51e6dab49469d643ced2c8fd2b6452796f8ae"
 
     url "https://cdn.kagi.com/updates/13_0/#{version.csv.second}.zip"
@@ -44,7 +44,7 @@ cask "orion" do
     end
   end
   on_sonoma do
-    version "1.1,148"
+    version "1.1.1,149"
     sha256 "816ee0d7e6288eda107e189c6c7fadfc0a5012aa21247007bd51321c40fa376a"
 
     url "https://cdn.kagi.com/updates/14_0/#{version.csv.second}.zip"
@@ -55,7 +55,7 @@ cask "orion" do
     end
   end
   on_sequoia do
-    version "1.1,148"
+    version "1.1.1,149"
     sha256 "572be3565129e83b6994feeddca871a4ae17e7efe084503b5740004deebf9e45"
 
     url "https://cdn.kagi.com/updates/15_0/#{version.csv.second}.zip"
@@ -66,8 +66,8 @@ cask "orion" do
     end
   end
   on_tahoe :or_newer do
-    version "1.1,148"
-    sha256 "1bb2005787d9eb7bbc7bd79d38e3ffe41a0e29c46d7d4a6c6a68fac832190555"
+    version "1.1.1,149"
+    sha256 "0b49ad18d13d3abd1a9457b61aee0b91170732f93544b5d9fe6528fd7b560768"
 
     url "https://cdn.kagi.com/updates/26_0/#{version.csv.second}.zip"
 
